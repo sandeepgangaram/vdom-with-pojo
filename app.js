bd1755b610a1b60d53806bae4b2c84d458ab821f
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2023-03-15 10:59:55 +0530

    add recursive dom update logic

--- app.js
+++ app.js
@@ -12,3 +12,22 @@ const markup = {
     },
   ],
 };
+
+const rootElement = document.getElementById("root");
+
+const makeElements = function (pojoElement, parentNode) {
+  const newNode =
+    pojoElement.type === "text"
+      ? document.createTextNode(pojoElement.value)
+      : document.createElement(pojoElement.type);
+
+  if (pojoElement.children) {
+    pojoElement.children.forEach((child) => {
+      makeElements(child, newNode);
+    });
+  }
+
+  console.log(newNode);
+
+  parentNode.appendChild(newNode);
+};
