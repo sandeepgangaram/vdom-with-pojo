f3f1271c0d60a9899dbb20ff2bd6ff9b041a7a6d
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2023-03-15 10:54:58 +0530

    add basic markup

---
+++ app.js
@@ -0,0 +1,14 @@
+const markup = {
+  type: "main",
+  children: [
+    {
+      type: "h2",
+      children: [
+        {
+          type: "text",
+          value: "Hello World!",
+        },
+      ],
+    },
+  ],
+};
