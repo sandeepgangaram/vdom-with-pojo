e6eb08e8888f684db0d732a19f5dd2d1873dc040
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2023-03-16 22:25:28 +0530

    add child elements

--- react-app.js
+++ react-app.js
@@ -3,5 +3,11 @@ const root = ReactDOM.createRoot(rootNode);
 root.render(React.createElement(App));
 
 function App() {
-  return React.createElement("button", null, "Clcik Me!");
+  return React.createElement(
+    "aside",
+    null,
+    React.createElement("h2", null, "Counter"),
+    React.createElement("p", null, "Clicked 1 times"),
+    React.createElement("button", null, "Click Me!")
+  );
 }
